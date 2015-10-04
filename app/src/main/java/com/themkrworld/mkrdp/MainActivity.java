63b6b7df2483130830805507c9d1dcfd834a93d0
manishrewalliya <manish.rewalliya@delhivery.com>
2015-10-04 16:33:02 +0530

    Android Studio Commit

---
+++ app/src/main/java/com/themkrworld/mkrdp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.themkrworld.mkrdp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mkr_dp_sample);
+    }
+}
